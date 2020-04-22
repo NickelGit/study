2522219eaec27d36599413ddc450779badf05710
NickelGit <old_nickel@mail.ru>
2020-04-22 10:44:47 +0400

    Array exercise complited

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,34 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        result_array = []
+        max = array.max
+        array.each do |x|
+          result_array << (x.positive? ? max : x)
+        end
+        result_array
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.empty?
+
+        search_index = array.length / 2
+        left_search_border = 0
+        right_search_border = array.length - 1
+        loop do
+          current_element = array[search_index]
+          return search_index if current_element == query
+
+          return -1 if search_index == right_search_border || search_index == left_search_border
+
+          if query > current_element
+            left_search_border = search_index
+            search_index += ((right_search_border - left_search_border) + 1) / 2
+          else
+            right_search_border = search_index
+            search_index -= ((right_search_border - left_search_border) + 1) / 2
+          end
+        end
       end
     end
   end
