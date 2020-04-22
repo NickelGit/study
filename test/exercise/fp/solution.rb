fd143b2d8e772d014c0c2e2161594dc95c75857c
NickelGit <old_nickel@mail.ru>
2020-04-22 19:44:06 +0400

    fp exercise complited

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,19 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rated_and_several_countries?(film)
+        film['country'].to_s.include?(',') &&
+          !film['rating_kinopoisk'].nil? &&
+          film['rating_kinopoisk'].to_f.positive?
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def rating(array)
+        rating_array = array.select { |film| rated_and_several_countries? film }.map { |film| film['rating_kinopoisk'].to_f }
+        rating_array.reduce(:+).to_f / rating_array.length
+      end
+
+      def chars_count(films, threshold)
+        films.select { |film| film['rating_kinopoisk'].to_f > threshold }.map { |film| film['name'].count 'и' }.reduce(:+)
       end
     end
   end
