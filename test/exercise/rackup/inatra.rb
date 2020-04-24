326f8901098e5cd0b4972b1489fdd023d2e63c14
NickelGit <old_nickel@mail.ru>
2020-04-24 16:46:22 +0400

    Inatra exercise comlited

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,19 @@
 module Inatra
+  @routes = {}
+
   class << self
-    def routes
+    def routes(&block)
+      instance_eval(&block)
+    end
+
+    def get(path, &block)
+      @routes['GET'] = { path => block }
     end
 
     def call(env)
+      request_method = env['REQUEST_METHOD']
+      path = env['PATH_INFO']
+      @routes[request_method][path].call
     end
   end
 end
