4dfd6d64853c1846dbadeebdf99de064ce136b6e
NickelGit <old_nickel@mail.ru>
2020-04-29 18:35:14 +0400

    New tests added to Inatra

--- test/exercise/rackup/test.rb
+++ test/exercise/rackup/test.rb
@@ -26,4 +26,25 @@ class RackTest < Test::Unit::TestCase
     assert @browser.last_response.ok?
     assert_equal 'Hello World! POST', @browser.last_response.body
   end
+
+  def test_it_pongs
+   
+    @browser.get '/ping'
+    assert @browser.last_response.ok?
+    assert_equal 'PONG', @browser.last_response.body
+  end
+
+  def test_it_says_bye
+   
+    @browser.post '/bye'
+    assert @browser.last_response.ok?
+    assert_equal 'Bye Bye', @browser.last_response.body
+  end
+
+  def test_it_handles_404
+   
+    @browser.get '/missing_method'
+    assert @browser.last_response.not_found?
+    assert_equal 'Not Found', @browser.last_response.body
+  end
 end
